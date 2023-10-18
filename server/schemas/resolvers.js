1dadb6390b6dcd598ad07263e40e78f921ddee78
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 09:26:39 -0500

    added login and register mutation code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,16 @@
 const { Users, Tech } = require('../models');
 
+const { ApolloError } = require('apollo-server-errors');
+
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
+
+
+
+
+
 const resolvers = {
     Query: {
         tech: async () => {
@@ -11,19 +22,59 @@ const resolvers = {
         }
     },
     Mutation: {
-        createUsers: async (parents, args) => {
-            const users = await Users.create(args);
-            return users;
-        },
-        createVote: async (parent, { _id, techNum }) => {
-            const vote = await Users.findOneAndUpdate(
-                { _id },
-                { $inc: { [`tech${techNum}_votes`]: 1 } },
-                { new: true }
+        async registerUser(_, {registerInput: {email, password}}) {
+            const oldUser = await Users.findOne({ email });
+            if (oldUser) {
+                throw new ApolloError('A user is already registered with the email' = email, 'USER_ALREADY_EXISTS');
+            }
+            const encryptedPassword = await bcrypt.hash(password, 10);
+
+            const newUser = new User({
+                email: email.toLowerCase(),
+                password: encryptedPassword
+            })
+
+            const token = jwt.sign(
+                { user_id: newUser._id, email },
+                '',
+                {
+                    expiresIn: '7h'
+                }
             );
-            return vote;
+
+            newUser.token = token; 
+
+            const res = await newUser.save();
+
+            return {
+                id: res.id,
+                ...res._doc
+            };
+        },
+        async loginUser(_, {loginInput: {email, password}}) {
+            const User = await Users.findOne({ email });
+
+            if (user && (await bcrypt.compare(password, user.password))) {
+
+                const token = jwt.sign(
+                    { user_id: newUser._id, email },
+                    'UNSAFE_STRING',
+                    {
+                        expiresIn: '7h'
+                    }
+                );
+
+                user.token = token;
+
+                return {
+                    id: user.id,
+                    ...user._doc
+                }
+            } else {
+                throw new ApolloError('Incorrect password', 'INCORRECT_PASSWORD');
+            }
         }
     }
-};
+}
 
 module.exports = resolvers;
