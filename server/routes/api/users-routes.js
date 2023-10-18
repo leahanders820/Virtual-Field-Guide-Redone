b4ff9b00786a450fb37d2c1eba1ee21a5ce964f9
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 15:51:43 -0500

    removed unecessary things

--- server/routes/api/users-routes.js
+++ server/routes/api/users-routes.js
@@ -3,12 +3,12 @@ const {
   getAllusers,
   createUsers,
   getUsers,
-  createVote,
+
 } = require('../../controllers/users-controller');
 
 router.route('/').get(getAllusers);
 router.route('/').post(createUsers);
 router.route('/:id').get(getUsers);
-router.route('/:id').put(createVote);
+
 
 module.exports = router;
