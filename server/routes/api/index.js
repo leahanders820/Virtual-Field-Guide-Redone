bf46db61ad8ef9d1466144815323d8a72dd8d977
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 09:26:56 -0500

    removed unneeded things

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const usersRoutes = require('./users-routes');
-const techRoutes = require('./tech-routes.js');
+
 
 router.use('/users', usersRoutes);
-router.use('/tech', techRoutes);
+
 
 module.exports = router;
