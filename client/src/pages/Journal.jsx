fdf7b4a03b01212ee410a10ebaa4e26de45b010b
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 09:25:42 -0500

    added visual content to journal page

--- client/src/pages/Journal.jsx
+++ client/src/pages/Journal.jsx
@@ -1,16 +1,45 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 // Uncomment import statements below after building queries and mutations
-import { useMutation, useQuery } from '@apollo/client';
-import { QUERY_TECH } from '../utils/queries';
-import { CREATE_MATCHUP } from '../utils/mutations';
+import { useMutation, useQuery } from "@apollo/client";
+
 
 const Journal = () => {
-  
-
   return (
-   <></>
+    <>
+    <div>
+      <ul id='continentlist' className="text-white">
+        <li>North America</li>
+        <li>South America</li>
+        <li>Africa</li>
+        <li>Europe</li>
+        <li>Asia</li>
+        <li>Oceania</li>
+        <li>Antarctica</li>
+      </ul>
+    </div>
+      <div className="card bg-white card-rounded w-25">
+        <div className="card-header bg-dark text-center">
+          <h1>Animal</h1>
+        </div>
+        <div className="card-body m-5 text-center"></div>
+      </div>
+
+      <div className="card bg-white card-rounded w-25">
+        <div className="card-header bg-dark text-center">
+          <h1>Plant</h1>
+        </div>
+        <div className="card-body m-5 text-center"></div>
+      </div>
+
+      <div className="card bg-white card-rounded w-25">
+        <div className="card-header bg-dark text-center">
+          <h1>Landmark</h1>
+        </div>
+        <div className="card-body m-5 text-center"></div>
+      </div>
+    </>
   );
 };
 
