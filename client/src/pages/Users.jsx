0a3124791a7b368fa3c470af69560300a7f122e8
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-17 23:22:22 -0500

    userpage layout and verbage

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 // Uncomment import statements below after building queries and mutations
-import { useMutation, useQuery } from '@apollo/client';
-import { QUERY_TECH } from '../utils/queries';
-import { CREATE_USERS } from '../utils/mutations';
+import { useMutation, useQuery } from "@apollo/client";
+import { QUERY_TECH } from "../utils/queries";
+import { CREATE_USERS } from "../utils/mutations";
 
 const Users = () => {
-  const {loading, data} = useQuery(QUERY_TECH);
+  const { loading, data } = useQuery(QUERY_TECH);
 
   const techList = data?.tech || [];
 
   const [formData, setFormData] = useState({
-    tech1: 'JavaScript',
-    tech2: 'JavaScript',
+    tech1: "one",
+    tech2: "two",
   });
   let navigate = useNavigate();
 
-  const [createUsers, {error}] = useMutation(CREATE_USERS);
+  const [createUsers, { error }] = useMutation(CREATE_USERS);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,8 +28,8 @@ const Users = () => {
     event.preventDefault();
 
     try {
-      const {data} = await createUsers({
-        variables: {...formData},
+      const { data } = await createUsers({
+        variables: { ...formData },
       });
 
       navigate(`/users/${data.createUsers._id}`);
@@ -38,45 +38,48 @@ const Users = () => {
     }
 
     setFormData({
-      tech1: 'JavaScript',
-      tech2: 'JavaScript',
+      tech1: "JavaScript",
+      tech2: "JavaScript",
     });
   };
 
   return (
     <div className="card bg-white card-rounded w-25">
       <div className="card-header bg-dark text-center">
-        <h1>Let's create a users!</h1>
+        <h1>Create an Account</h1>
       </div>
-      <div className="card-body m-5">
+      <div className="card-body m-5 text-center">
         {loading ? (
           <div>Loading ...</div>
-        ) :
-        <form onSubmit={handleFormSubmit}>
-          <label>Tech 1: </label>
-          <select name="tech1" onChange={handleInputChange}>
-            {techList.map((tech) => {
-              return (
-                <option key={tech._id} value={tech.name}>
-                  {tech.name}
-                </option>
-              );
-            })}
-          </select>
-          <label>Tech 2: </label>
-          <select name="tech2" onChange={handleInputChange}>
-            {techList.map((tech) => {
-              return (
-                <option key={tech._id} value={tech.name}>
-                  {tech.name}
-                </option>
-              );
-            })}
-          </select>
-          <button className="btn btn-danger" type="submit">
-            Create Users!
-          </button>
-        </form>}
+        ) : (
+          <form onSubmit={handleFormSubmit}>
+            <label>Email </label>
+            <input type='email'></input>
+            {/* <select name="tech1" onChange={handleInputChange}>
+              {techList.map((tech) => {
+                return (
+                  <option key={tech._id} value={tech.name}>
+                    {tech.name}
+                  </option>
+                );
+              })}
+            </select> */}
+            <label>Password </label>
+            <input type='password'></input>
+            {/* <select name="tech2" onChange={handleInputChange}>
+              {techList.map((tech) => {
+                return (
+                  <option key={tech._id} value={tech.name}>
+                    {tech.name}
+                  </option>
+                );
+              })}
+            </select> */}
+            <button className="btn btn-info" type="submit">
+              Sign Up
+            </button>
+          </form>
+        )}
       </div>
       {error && <div>Something went wrong!</div>}
     </div>
