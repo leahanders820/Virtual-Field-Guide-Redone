62dbfc0b2420a1363a7ce35b9f80a7b99ff5ab47
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 08:03:28 -0500

    removed code from journal.jsx

---
+++ client/src/pages/Journal.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Uncomment import statements below after building queries and mutations
+import { useMutation, useQuery } from '@apollo/client';
+import { QUERY_TECH } from '../utils/queries';
+import { CREATE_MATCHUP } from '../utils/mutations';
+
+const Journal = () => {
+  
+
+  return (
+   <></>
+  );
+};
+
+export default Journal;
