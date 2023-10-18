2aee2cf25b5aeb6c079a362f775471c5910dcaf8
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 12:50:35 -0500

    user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,10 +6,9 @@ export const QUERY_USERS = gql`
   query users($_id: String) {
     users(_id: $_id) {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+      email
+      
+      
     }
   }
 `;
