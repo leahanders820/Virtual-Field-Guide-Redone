7bdce569ea5c7a00c81363172101fe8c6aad2290
Leah Anderson <leahanderson@Leahs-MacBook-Pro-2.local>
2023-10-18 09:27:09 -0500

    user schema

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,24 +1,11 @@
 const { Schema, model } = require('mongoose');
 
-const userschema = new Schema({
-  tech1: {
-    type: String,
-    required: true,
-  },
-  tech2: {
-    type: String,
-    required: true,
-  },
-  tech1_votes: {
-    type: Number,
-    default: 0,
-  },
-  tech2_votes: {
-    type: Number,
-    default: 0,
-  },
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: { type: String },
+  token: { type: String }
 });
 
-const Users = model('Users', userschema);
+const Users = model('Users', userSchema);
 
 module.exports = Users;
